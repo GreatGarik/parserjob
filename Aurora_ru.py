811d89e941ca49b1276bdc612c5b6a6b3a49d821
Igor Perovsky <greatgarik@mail.ru>
2023-02-07 16:48:48 +0300

    Рабочая версия.

--- Aurora_ru.py
+++ Aurora_ru.py
@@ -14,8 +14,8 @@ with open(file_name, 'w', encoding='utf-8', newline='') as file_csv:
     writer.writerow(['Наименование', 'Артикул', 'Цена', 'Описание', 'Характеристики', 'Ссылка'])
 
 # Получение URL товаров со странички
-# url = input('Введите URL для опроса')
-url = f'https://aurora.ru/catalog/category/aksessuary-dlya-shitya/'
+url = input('Введите URL для опроса:    ')
+#url = f'https://aurora.ru/catalog/category/aksessuary-dlya-shitya/'
 response = requests.get(url=url, headers=header)
 response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text, 'lxml')
@@ -23,7 +23,6 @@ soup = BeautifulSoup(response.text, 'lxml')
 # Получаем список URL
 links = [i.next['href'] for i in soup.find_all('div', class_="catalog_item_name")]
 
-links = ['/catalog/goods/Nitki-shveynie-Cotton--503-Aurora/1099/']
 file = open(file_name, 'a', encoding='utf-8', newline='')
 for link in links:
     link = f'https://aurora.ru{link}'
@@ -32,7 +31,7 @@ for link in links:
     soup = BeautifulSoup(response.text, 'lxml')
 
     name = soup.find('h1', class_="goods_h1_name").text.strip()
-    article = soup.find('div', class_="goods_artikul").text.split(':')[1].strip() if soup.find('div', class_="goods_artikul") and len(soup.find('div', class_="goods_artikul").text.split(':')) > 1 else f'Нет артикула {name}'.replace('/', '-')
+    article = soup.find('div', class_="goods_artikul").text.split(':')[1].strip().replace('*', '-') if soup.find('div', class_="goods_artikul") and len(soup.find('div', class_="goods_artikul").text.split(':')) > 1 else f'Нет артикула {name}'.replace('/', '-')
     price = soup.find('span', class_="price").text.split()[0].strip() if soup.find('span', class_="price") else 'Нет цены'
     description = soup.find('div', id="description").text.strip()
     characteristics_names = [i.text for i in soup.find_all('span', class_="param_name")]
@@ -42,14 +41,16 @@ for link in links:
 
 
     if not os.path.exists(f'Aurora_ru/{article}'):
-        images = [i['href'] for i in soup.find_all('a', class_="goods_gallery cboxElement")]
+        images = [i['href'] for i in soup.find_all('a', class_="goods_gallery")]
         os.mkdir(f'Aurora_ru/{article}')
         for image in images:
-            response = requests.get(url=image, stream=True)
-            with open(f'Aurora_ru/{article}/{image.split("/")[-1]}', 'wb') as f:
-                f.write(response.content)
+            try:
+                response = requests.get(url=f'https://aurora.ru{image}', stream=True)
+                with open(f'Aurora_ru/{article}/{image.split("/")[-1]}', 'wb') as f:
+                        f.write(response.content)
+            except Exception as e:
+                print(e)
 
-#    for name, article, price, description, characteristic, link1 in zip(names, articles, prices, descriptions, characteristics, links):
     flatten = name, article, price, description, ', '.join(characteristics), link
     writer = csv.writer(file, delimiter=';')
     writer.writerow(flatten)
