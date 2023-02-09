df253c8d932d235282c1aa7f3336e96e25f77fc4
Igor Perovsky <greatgarik@mail.ru>
2023-02-09 15:02:50 +0300

    Рабочая версия.

--- Aurora_ru.py
+++ Aurora_ru.py
@@ -4,6 +4,7 @@ import csv
 import os
 import time
 
+
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0',
           'Accept': 'ext/html,application/xhtml+txml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'}
 
